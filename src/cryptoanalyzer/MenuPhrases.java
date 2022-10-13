f277083af6b50163a8fe1f4922f46b92a6d21258
Iaroslav Bortkevich <ispaanec@gmail.com>
2022-10-13 22:31:50 +0300

    Final

---
+++ src/cryptoanalyzer/MenuPhrases.java
@@ -0,0 +1,19 @@
+package cryptoanalyzer;
+
+public class MenuPhrases {
+    public static final String ALPHABET = "אבגדהו¸זחטיךכלםמןנסעףפץקצרשתûü‎‏ÿ.,:-!?\" ";
+
+    public static final int OFFSET_NUMBER = 3;
+
+    public static final String WELCOME_PHRASE = "Welcome to CryptoAnalyzer!\n" +
+            "This program work with Russian alphabet and special symbols (.,:-!?\"SPACE)\n";
+    public static final String METHOD_CHOICE = "What do you want ? Press any key:\n" +
+            " 1 - Cipher text  \n 2 - Decipher text \n 3 - Brute Force text file \n 4 - Exit\n";
+
+    public static final String SECOND_METHOD_CHOICE = "Choose method \n 5 - Work with console \n 6 - Work with File.txt ";
+
+    public static final String WRONG_KEY = "You press wrong key";
+
+    public static final String CHECK = "Check your new FileCopy in directory ";
+
+}
